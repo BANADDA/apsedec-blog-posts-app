e8444b6bd21ae3e1a0519084508f6b87b39ccece
BANADDA MUBARAKA <83466862+BANADDA@users.noreply.github.com>
2024-08-14 06:20:39 +0300

    modified:   src/widgets/BlogCard.js

--- src/widgets/BlogCard.js
+++ src/widgets/BlogCard.js
@@ -8,31 +8,32 @@ import React from 'react';
 
 const BlogCard = ({ imageUrl, title, createdAt, caption, onView, onEdit, onDelete }) => {
     const date = createdAt instanceof Date ? createdAt : createdAt.toDate();
-  return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        sx={{ height: 140 }}
-        image={imageUrl}
-        title={title}
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {title}
-        </Typography>
-                <Typography variant="body2" color="text.secondary" sx={{ mb: 2 }}>
-                {date.toLocaleDateString(undefined, { year: 'numeric', month: 'long', day: 'numeric' })}
+  
+    return (
+        <Card sx={{ maxWidth: 345, height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+            <CardMedia
+                sx={{ height: 140 }}
+                image={imageUrl}
+                title={title}
+            />
+            <CardContent sx={{ flexGrow: 1 }}>
+                <Typography gutterBottom variant="h5" component="div">
+                    {title}
                 </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {caption}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small" onClick={onView}>View</Button>
-        <Button size="small" onClick={onEdit}>Edit</Button>
-        <Button size="small" onClick={onDelete}>Delete</Button>
-      </CardActions>
-    </Card>
-  );
+                <Typography variant="body2" color="text.secondary" sx={{ mb: 2 }}>
+                    {date.toLocaleDateString(undefined, { year: 'numeric', month: 'long', day: 'numeric' })}
+                </Typography>
+                <Typography variant="body2" color="text.secondary" sx={{ mb: 2, overflow: 'hidden', textOverflow: 'ellipsis', display: '-webkit-box', WebkitLineClamp: 3, WebkitBoxOrient: 'vertical' }}>
+                    {caption}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small" onClick={onView}>View</Button>
+                <Button size="small" onClick={onEdit}>Edit</Button>
+                <Button size="small" onClick={onDelete}>Delete</Button>
+            </CardActions>
+        </Card>
+    );
 };
 
 export default BlogCard;
